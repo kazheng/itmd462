a00b1c5aa6438ddf4949630169d31e1d788709a4
kazheng <kzheng8@hawk.iit.edu>
2016-01-19 21:16:17 -0600

    First commit of code for project delphi

---
+++ src/AppBundle/Controller/NoteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class NoteController
+{
+    /**
+     * @Route('/note');
+     */
+    public function showAction()
+    {
+        return new Response('HELLO!');
+    }
+}
